81ffeea40a2cc01c10f9ca3b1edc4f55b0832531
Seto Apriawan <Setoapriawan69@gmail.com>
2022-09-13 15:36:48 +0700

    Pertemuan 2

---
+++ src/percabangan.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class percabangan {
+    public static void main(String[] args) {
+        String nama, keterangan;
+        int nilai;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Masukan Nama : ");
+        nama= input.next();
+        System.out.println("Masukan Nilai : ");
+        nilai= input.nextInt();
+        if (nilai>=60){
+            keterangan="Lulus";
+            System.out.println(nama+""+keterangan);
+
+        }
+        else{
+            keterangan="Tidak Lulus";
+            System.out.println(nama+" "+keterangan);
+        }
+
+
+    }
+}
