c831c815e1d56e1a96cad97e35e72de9bd64e500
Seto <setoapriawan69@gmail.com>
2022-10-04 15:38:03 +0700

    Pertemuan 6

--- src/Project/Siakad.java
+++ src/Project/Siakad.java
@@ -1,11 +1,51 @@
 package Project;
+import java.util.Scanner;
 
 public class Siakad {
+    public static void tambahData(){
+        System.out.println("Silahkan Tambah Data");
+    }
+    public static void lihatData(){
+        System.out.println("Berikut data anda");
+    }
     public static void main(String[] args) {
-        Mahasiswa x=new Mahasiswa();
-        x.setNIM(1);
-        x.setNama("A");
-        System.out.println(x.getNim());
-        System.out.println(x.getNama());
+        Scanner input=new Scanner(System.in);
+        /*System.out.println("Menu Siakad");
+        System.out.println("1. Tambah Data ");
+        System.out.println("2. Lihat Data ");
+        System.out.println("3. Keluar \n Pilih Menu = ");
+        int menu=input.nextInt();*/
+        do {
+            System.out.println("Menu Siakad");
+            System.out.println("1. Tambah Data ");
+            System.out.println("2. Lihat Data ");
+            System.out.println("3. Keluar \n Pilih Menu = ");
+            int menu=input.nextInt();
+            if(menu==1){
+                tambahData();
+
+            }
+            else if(menu==2){
+                lihatData();
+            }
+            else if(menu==3){
+                break;
+            }
+            else{
+
+            }
+
+
+        }while (true);
+        /*switch(menu){
+            case 1:
+                System.out.println("Silahkan tambah data");
+                break;
+            case 2:
+                System.out.println("Berikut data anda ");
+                break;
+            case 3:
+            break;
+        }*/
     }
 }
