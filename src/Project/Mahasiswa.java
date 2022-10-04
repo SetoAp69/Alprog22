7b4bf1008b677c11bb4a15674b2ce60695b4d305
Seto <setoapriawan69@gmail.com>
2022-10-04 14:44:01 +0700

    Pertemuan 6

---
+++ src/Project/Mahasiswa.java
@@ -0,0 +1,23 @@
+package Project;
+
+public class Mahasiswa {
+    private String nama;
+    private int nim;
+    /*public Mahasiswa(String nama,int Nim){
+        this.nama=nama;
+        this.nim=Nim;
+    }*/
+    public int getNim(){
+        return this.nim;
+    }
+    public void setNIM(int nim){
+        this.nim=nim;
+    }
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    public String getNama(){
+        return this.nama;
+    }
+
+}
---
+++ src/Project/Siakad.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Siakad {
+    public static void main(String[] args) {
+        Mahasiswa x=new Mahasiswa();
+        x.setNIM(1);
+        x.setNama("A");
+        System.out.println(x.getNim());
+        System.out.println(x.getNama());
+    }
+}
